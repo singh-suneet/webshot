8de81cc27e850b23448d7aa2bf6bb1a9a139d2d4
Suneet Singh <62961821+singh-suneet@users.noreply.github.com>
2021-01-19 22:10:06 +0530

    Add files via upload

--- webshot.py
+++ webshot.py
@@ -1,18 +1,29 @@
 # made by R4J4
 #Require selenium
 #Require chromewebdriver
-
+import random
 from selenium import webdriver
+import re
+def banner():
+    banner=""" 
+               *******************************
+               ************Web-Shot***********
+               *******************************"""
+    return banner
 
-ip=list(input("enter list").split())
+
+print(banner()+"\n"+"Welcome to Web-Shot, A simple Script to take screenshot of multiple IP addresses or domains. \n")
+
+ip=list(re.split(',|\s', input("Enter either comma separated or space separated list of IPs or domains:\t"),))
 print(ip)
 
-for i in ip:
+def shot(ip):
+ for i in ip:
     add = "https://" + i
     print(add)
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
-
     options = webdriver.ChromeOptions()
+    options.add_argument('--disable-logging')
     options.headless = True
     options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--window-size=1920,1080")
@@ -25,7 +36,10 @@ for i in ip:
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options) #add your chromedriver path
-    driver.get("%s" % add)
+    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options) # add your chromedriver or geckodriver path
+    driver.get("%s" %add)
     nam = i+'.png'
     driver.get_screenshot_as_file(nam)
+    print("Screenshot taken \n")
+
+shot(ip)
